cf0d7d84a6b5ae43284de3d762ff19441e498dcd
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2024-05-27 22:43:26 +0600

    Add file.

---
+++ bootstrap/cache/frontend_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Frontend\\Providers\\FrontendServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Frontend\\Providers\\FrontendServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ bootstrap/cache/photo_gallery_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\PhotoGallery\\Providers\\PhotoGalleryServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\PhotoGallery\\Providers\\PhotoGalleryServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
