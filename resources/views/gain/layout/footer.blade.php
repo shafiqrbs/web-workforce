f9f21904b3de9429b0ea75d5bf2c3739926b697e
rbraju3m <rbraju3m@gmail.com.com>
2025-07-15 14:48:22 +0600

    country map

--- resources/views/gain/layout/footer.blade.php
+++ resources/views/gain/layout/footer.blade.php
@@ -23,14 +23,7 @@
                     <li><a href="{{route('cms.events')}}">Events</a></li>
                 </ul>
             </div>
-            <div class="col-lg-2">
-                <h6>Services</h6>
-                <ul class="list-unstyled">
-                    <li><a href="https://www.gainhealth.org/" target="_blank">Global Alliance for Improved Nutrition</a></li>
-                    <li><a href="https://www.gainhealth.org/impact/programmes/workforce-nutrition" target="_blank">Workforce Nutrition Alliance</a></li>
-                    <li><a href="https://play.google.com/store/apps/details?id=com.fairshop.pos&hl=en&gl=US" target="_blank">Fair Shop</a></li>
-                </ul>
-            </div>
+
             <div class="col-lg-2">
                 <h6>Resources</h6>
                 <ul class="list-unstyled">
@@ -40,12 +33,22 @@
                 </ul>
             </div>
             <div class="col-lg-2">
+                <h6>Services</h6>
+                <ul class="list-unstyled">
+                    <li><a href="https://www.gainhealth.org/" target="_blank">Global Alliance for Improved Nutrition</a></li>
+                    <li><a href="https://www.gainhealth.org/impact/programmes/workforce-nutrition" target="_blank">Workforce Nutrition Alliance</a></li>
+                    <li><a href="https://play.google.com/store/apps/details?id=com.fairshop.pos&hl=en&gl=US" target="_blank">Fair Shop</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2">
                 <h6>Support</h6>
                 <ul class="list-unstyled">
-                    <li><a href="{{$siteSetting->facebook_address}}" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a></li>
-                    <li><a href="{{$siteSetting->youtube_address}}" target="_blank"><i class="fab fa-youtube"></i> Youtube</a></li>
-                    <li><a href="{{$siteSetting->linkedin_address}}" target="_blank"><i class="fab fa-linkedin-in"></i> Linkedin</a></li>
-                    <a href="" target="_blank"><i class="fab fa-instagram"></i> Instagram</a>
+                    <li><a href="https://cqi.workforcenutritionbd.org" target="_blank">Swapno</a></li>
+                    <li><a href="https://cqi.workforcenutritionbd.org/swapno" target="_blank">Swapno Overview</a></li>
+                    <li><a href="https://drive.google.com/file/d/1eYiP02b-bjZks9NPGllQF1qdjx4XU344/view" target="_blank">CQI</a></li>
+{{--                    <li><a href="{{$siteSetting->youtube_address}}" target="_blank"><i class="fab fa-youtube"></i> Youtube</a></li>--}}
+{{--                    <li><a href="{{$siteSetting->linkedin_address}}" target="_blank"><i class="fab fa-linkedin-in"></i> Linkedin</a></li>--}}
+{{--                    <a href="" target="_blank"><i class="fab fa-instagram"></i> Instagram</a>--}}
                 </ul>
             </div>
         </div>
